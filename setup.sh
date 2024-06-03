44b9be136af27d7449e8902ff0d35992182fc619
Young Yu <youngyu19@gmail.com>
2024-06-02 22:47:06 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,16 +7,16 @@ DOTFILES=".zshrc"
 echo "Creating symlinks for each dotfile..."
 for file in $DOTFILES; do
     if [ -f ~/$file ] || [ -L ~/$file ]; then
-        echo "Removing existing $file"
+        echo "Removing existing $file..."
         rm -f ~/$file
     fi
-    echo "Creating symlink for $file"
+    echo "Creating symlink for $file..."
     ln -s ~/dotfiles/$file ~/$file
 done
 
 # Source the .zshrc file to apply new changes
 if [ -f .zshrc ]; then
-    echo "Sourcing .zshrc to apply changes"
+    echo "Sourcing .zshrc to apply changes..."
     source .zshrc
 else
     echo ".zshrc file not found!"
