17e7f2b6616151f9ad7594b82cd8b5a701d3b8b7
Young Yu <youngyu19@gmail.com>
2024-06-04 23:25:22 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ for file in $DOTFILES; do
         rm -f ~/$file
     fi
     echo "Creating symlink for $file..."
-    ln -s ~/dotfiles/$file ~/$file
+    ln -s "$(pwd)/.zshrc" ~/.zshrc
 done
 
 # Source the .zshrc file to apply new changes
